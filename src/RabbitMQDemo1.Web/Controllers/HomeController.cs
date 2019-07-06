35b4c5c9d125c9de0bd0856a1ea5a842dfa7b7ac
Rodrigo Martins de Freitas Alves <rodrigo.martins.50@hotmail.com>
2019-07-06 13:29:17 -0300

    Adicionando projetos e docker-compose.

---
+++ src/RabbitMQDemo1.Web/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQDemo1.Web.Models;
+
+namespace RabbitMQDemo1.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string texto)
+        {
+
+            return Ok(true);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
